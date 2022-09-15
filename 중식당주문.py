170085887f7746390d934b417005739b6023358e
moksu27 <qotnahr@naver.com>
2022-09-15 09:47:10 +0900

    day2

---
+++ 중식당주문.py
@@ -0,0 +1,7 @@
+#orders = ['짜장','짬뽕','탕수육']
+#del orders [0]
+
+#print(len(orders))
+
+name='안녕하세요 코드라이언입니다.'
+print(len(name))
---
+++ 친구야안녕.py
@@ -0,0 +1,7 @@
+food1 = input("내가 먹은 음식의 가격 : ")
+food2 = input("친구가 먹은 음식의 가격 : ")
+
+food1 = int(food1)
+food2 = int(food2)
+
+print(food1 + food2)
